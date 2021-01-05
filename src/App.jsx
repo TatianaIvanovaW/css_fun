6429c9f14ddfc86126d82e7e4716f49067589e71
Tatiana Ivanova <ivanova.tatiana.w@gmail.com>
2021-01-05 19:14:21 +0100

    fullscreen menu

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import cx from "classnames";
+import FullscreenMenu from "./FullscreenMenu";
+
+export default function App() {
+  const [open, set_open] = useState(false);
+  return (
+    <div>
+      <FullscreenMenu />
+      <div
+        className={cx("searchbar", {
+          "is-open": open,
+        })}
+      >
+        <input
+          placeholder="Search"
+          onFocus={(e) => set_open(true)}
+          onBlur={(e) => set_open(false)}
+        />
+        <div className="dropdown_positioner">
+          <div className="dropdown">
+            <ul>
+              <li>
+                <strong>re</strong>al life CSS transition
+              </li>
+              <li>
+                <strong>Re</strong>act hooks
+              </li>
+              <li>
+                pa<strong>re</strong>ntheses
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <p>Use the searchbar above</p>
+    </div>
+  );
+}
